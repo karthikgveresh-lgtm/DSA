dc220eabeaae8c96d105a78524b352c7f8090454
karthikgveresh-lgtm <karthikgveresh@gmail.com>
2026-02-27 12:29:44 +0530

    Add Move Zeros solution

---
+++ Move Zeros.java
@@ -0,0 +1,21 @@
+class main {
+    public static void main(String[] args) {
+        int nums[] = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
